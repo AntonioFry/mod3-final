a940d12c148ca53dc1c40069aea68d97f1126e04
Antonio Fry <antoniofry2000@gmail.com>
2019-07-31 09:28:31 -0600

    initialize files

---
+++ src/Form.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state ={
+
+    }
+  }
+
+  render() {
+    return (
+      <header>
+        <h1>Animal Rescue</h1>
+        <form>
+          <input
+          
+          />
+          <input
+
+          />
+          <button>Donate!</button>
+        </form>
+      </header>
+    )
+  }
+}
